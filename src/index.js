ebd06ea36ddb1ac3ca3fe7969edca94dd1b040b2
mohamadarvahi3-png <mohamadarvahi3@gmail.com>
2026-07-09 02:47:07 +0330

    Create src/index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is online!`);
+
+  client.user.setPresence({
+    activities: [
+      {
+        name: "Persian Life",
+        type: ActivityType.Playing
+      }
+    ],
+    status: "online"
+  });
+});
+
+client.login(process.env.TOKEN);

6d0043f645236448966d1bdb3cf069307417f772
mohamadarvahi3-png <mohamadarvahi3@gmail.com>
2026-07-09 10:00:18 -0700

    fix bot

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
-console.log("Bot is starting...");
-require("./server");
 require("dotenv").config();
-require("./deploy-commands");
-console.log("TOKEN:", process.env.TOKEN ? "OK" : "Missing");
-console.log("CLIENT_ID:", process.env.CLIENT_ID);
-console.log("GUILD_ID:", process.env.GUILD_ID);
-const {
-  Client,
-  GatewayIntentBits,
-  ActivityType
-} = require("discord.js");
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
-  ]
-});
-
-client.once("ready", () => {
-  console.log(`${client.user.tag} is online!`);
-
-  client.user.setPresence({
-    activities: [
-      {
-        name: "Persian Life",
-        type: ActivityType.Playing
-      }
-    ],
-    status: "online"
-  });
-});
-
-
-client.login(process.env.TOKEN);
-console.log(JSON.stringify(process.env.TOKEN));
+require("./server");
+const {Client,GatewayIntentBits,ActivityType,Events}=require("discord.js");
+const client=new Client({intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMembers]});
+client.once(Events.ClientReady,c=>{console.log(`${c.user.tag} is online!`);c.user.setPresence({activities:[{name:"Persian Life",type:ActivityType.Playing}],status:"online"});});
+client.login(process.env.TOKEN).catch(console.error);

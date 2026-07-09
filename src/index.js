bd045a7c4843c70923d3a2af628176570dca902f
mohamadarvahi3-png <mohamadarvahi3@gmail.com>
2026-07-09 23:32:13 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 require("dotenv").config();
 require("./server");
-const {Client,GatewayIntentBits,ActivityType,Events}=require("discord.js");
-const client=new Client({intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMembers]});
-client.once(Events.ClientReady,c=>{console.log(`${c.user.tag} is online!`);c.user.setPresence({activities:[{name:"Persian Life",type:ActivityType.Playing}],status:"online"});});
-client.login(process.env.TOKEN).catch(console.error);
+
+console.log("شروع برنامه");
+
+const {
+  Client,
+  GatewayIntentBits,
+  Events
+} = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once(Events.ClientReady, c => {
+  console.log(`${c.user.tag} آنلاین شد`);
+});
+
+client.login(process.env.TOKEN)
+  .then(() => console.log("Login موفق"))
+  .catch(err => console.error("Login Error:", err));

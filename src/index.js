a4f0a52318326bb0ab81ea820cb52c0515d84925
mohamadarvahi3-png <mohamadarvahi3@gmail.com>
2026-07-09 16:43:15 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ console.log("Bot is starting...");
 require("./server");
 require("./deploy-commands");
 require("dotenv").config();
-
+console.log("TOKEN:", process.env.TOKEN ? "OK" : "Missing");
+console.log("CLIENT_ID:", process.env.CLIENT_ID);
+console.log("GUILD_ID:", process.env.GUILD_ID);
 const {
   Client,
   GatewayIntentBits,
